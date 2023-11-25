efe666549c7fa1bbfcae24c176ef8360f34f1f1a
Codby <87335804+TheCodby@users.noreply.github.com>
2023-11-25 22:42:15 +0300

    initialized tcp server and spawn a new thread for each connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    io::{BufReader, Read, Result},
+    net::{TcpListener, TcpStream},
+    str::from_utf8,
+    thread,
+};
+fn handle_connection(mut stream: TcpStream) -> Result<()> {
+    let mut reader = BufReader::new(&mut stream);
+    let mut buffer = [0; 4096];
+    match reader.read(&mut buffer) {
+        Ok(n) => {
+            if n == 0 {
+                return Ok(());
+            }
+            println!("Received {} bytes: {:?}", n, from_utf8(&buffer[..n]));
+        }
+        Err(e) => {
+            eprintln!("Error reading from stream: {}", e);
+            return Err(e);
+        }
+    }
+
+    Ok(())
+}
+fn main() -> Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:3000")?;
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        println!("Connection established!");
+
+        thread::spawn(|| {
+            handle_connection(stream).unwrap_or_else(|error| {
+                eprintln!("Error handling connection: {}", error);
+            });
+        });
+    }
+    Ok(())
 }
